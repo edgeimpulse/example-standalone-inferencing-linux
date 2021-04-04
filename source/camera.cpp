54d3b2a10d6e1b3f90eaa94e6e1c61654cfbeceb
Jan Jongboom <janjongboom@gmail.com>
2021-04-04 10:30:39 +0200

    Image examples working for image classification & object detection

--- source/camera.cpp
+++ source/camera.cpp
@@ -1,9 +1,46 @@
 #include <unistd.h>
 #include "opencv2/opencv.hpp"
 #include "opencv2/videoio/videoio_c.h"
+#include "edge-impulse-sdk/classifier/ei_run_classifier.h"
 #include "iostream"
 
 static bool use_debug = false;
+
+// If you don't want to allocate this much memory you can use a signal_t structure as well
+// and read directly from a cv::Mat object, but on Linux this should be OK
+static float features[EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT];
+
+/**
+ * Resize and crop to the set width/height from model_metadata.h
+ */
+void resize_and_crop(cv::Mat *in_frame, cv::Mat *out_frame) {
+    // to resize... we first need to know the factor
+    float factor_w = static_cast<float>(EI_CLASSIFIER_INPUT_WIDTH) / static_cast<float>(in_frame->cols);
+    float factor_h = static_cast<float>(EI_CLASSIFIER_INPUT_HEIGHT) / static_cast<float>(in_frame->rows);
+
+    float largest_factor = factor_w > factor_h ? factor_w : factor_h;
+
+    cv::Size resize_size(static_cast<int>(largest_factor * static_cast<float>(in_frame->cols)),
+        static_cast<int>(largest_factor * static_cast<float>(in_frame->rows)));
+
+    cv::Mat resized;
+    cv::resize(*in_frame, resized, resize_size);
+
+    int crop_x = resize_size.width > resize_size.height ?
+        (resize_size.width - resize_size.height) / 2 :
+        0;
+    int crop_y = resize_size.height > resize_size.width ?
+        (resize_size.height - resize_size.width) / 2 :
+        0;
+
+    cv::Rect crop_region(crop_x, crop_y, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT);
+
+    if (use_debug) {
+        printf("crop_region x=%d y=%d width=%d height=%d\n", crop_x, crop_y, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT);
+    }
+
+    *out_frame = resized(crop_region);
+}
 
 int main(int argc, char** argv) {
     // If you see: OpenCV: not authorized to capture video (status 0), requesting... Abort trap: 6
@@ -30,26 +67,92 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // create a window to display the images from the webcam
-    cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
+    if (use_debug) {
+        // create a window to display the images from the webcam
+        cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
+    }
 
     // this will contain the image from the webcam
     cv::Mat frame;
 
     // display the frame until you press a key
     while (1) {
+        // 100ms. between inference
+        int64_t next_frame = (int64_t)(ei_read_timer_ms() + 100);
+
         // capture the next frame from the webcam
         camera >> frame;
 
-        printf("new frame\n");
+        cv::Mat cropped;
+        resize_and_crop(&frame, &cropped);
+
+        size_t feature_ix = 0;
+        for (int rx = 0; rx < (int)cropped.rows; rx++) {
+            for (int cx = 0; cx < (int)cropped.cols; cx++) {
+                cv::Vec3b pixel = cropped.at<cv::Vec3b>(rx, cx);
+                uint8_t b = pixel.val[0];
+                uint8_t g = pixel.val[1];
+                uint8_t r = pixel.val[2];
+                features[feature_ix++] = (r << 16) + (g << 8) + b;
+            }
+        }
+
+        ei_impulse_result_t result;
+
+        // construct a signal from the features buffer
+        signal_t signal;
+        numpy::signal_from_buffer(features, EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT, &signal);
+
+        // and run the classifier
+        EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false);
+        if (res != 0) {
+            printf("ERR: Failed to run classifier (%d)\n", res);
+            return 1;
+        }
+
+    #if EI_CLASSIFIER_OBJECT_DETECTION == 1
+        printf("Classification result (%d ms.):\n", result.timing.dsp + result.timing.classification);
+        bool found_bb = false;
+        for (size_t ix = 0; ix < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; ix++) {
+            auto bb = result.bounding_boxes[ix];
+            if (bb.value == 0) {
+                continue;
+            }
+
+            found_bb = true;
+            printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
+        }
+
+        if (!found_bb) {
+            printf("    no objects found\n");
+        }
+    #else
+        printf("%d ms. ", result.timing.dsp + result.timing.classification);
+        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+            printf("%s: %.05f", result.classification[ix].label, result.classification[ix].value);
+            if (ix != EI_CLASSIFIER_LABEL_COUNT - 1) {
+                printf(", ");
+            }
+        }
+        printf("\n");
+    #endif
 
         // show the image on the window
-        cv::imshow("Webcam", frame);
-        // wait (10ms) for a key to be pressed
-        if (cv::waitKey(10) >= 0)
-            break;
+        if (use_debug) {
+            cv::imshow("Webcam", cropped);
+            // wait (10ms) for a key to be pressed
+            if (cv::waitKey(10) >= 0)
+                break;
+        }
 
-        usleep(100 * 1000);
+        int64_t sleep_ms = next_frame > (int64_t)ei_read_timer_ms() ? next_frame - (int64_t)ei_read_timer_ms() : 0;
+        if (sleep_ms > 0) {
+            usleep(sleep_ms * 1000);
+        }
     }
     return 0;
 }
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA
+#error "Invalid model for current sensor."
+#endif
