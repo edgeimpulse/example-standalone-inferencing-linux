9982c5f861d49aabe9f569e9eeb5a4e3e350e412
Jan Jongboom <janjongboom@gmail.com>
2022-06-13 17:15:41 +0200

    Use full res video to display

--- source/camera.cpp
+++ source/camera.cpp
@@ -26,7 +26,7 @@
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
 #include "iostream"
 
-static bool use_debug = false;
+static bool use_debug = true;
 
 // If you don't want to allocate this much memory you can use a signal_t structure as well
 // and read directly from a cv::Mat object, but on Linux this should be OK
@@ -35,10 +35,10 @@ static float features[EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT];
 /**
  * Resize and crop to the set width/height from model_metadata.h
  */
-void resize_and_crop(cv::Mat *in_frame, cv::Mat *out_frame) {
+void resize_and_crop(cv::Mat *in_frame, cv::Mat *out_frame, int width, int height) {
     // to resize... we first need to know the factor
-    float factor_w = static_cast<float>(EI_CLASSIFIER_INPUT_WIDTH) / static_cast<float>(in_frame->cols);
-    float factor_h = static_cast<float>(EI_CLASSIFIER_INPUT_HEIGHT) / static_cast<float>(in_frame->rows);
+    float factor_w = static_cast<float>(width) / static_cast<float>(in_frame->cols);
+    float factor_h = static_cast<float>(height) / static_cast<float>(in_frame->rows);
 
     float largest_factor = factor_w > factor_h ? factor_w : factor_h;
 
@@ -55,10 +55,10 @@ void resize_and_crop(cv::Mat *in_frame, cv::Mat *out_frame) {
         (resize_size.height - resize_size.width) / 2 :
         0;
 
-    cv::Rect crop_region(crop_x, crop_y, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT);
+    cv::Rect crop_region(crop_x, crop_y, width, height);
 
     if (use_debug) {
-        printf("crop_region x=%d y=%d width=%d height=%d\n", crop_x, crop_y, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT);
+        // printf("crop_region x=%d y=%d width=%d height=%d\n", crop_x, crop_y, width, height);
     }
 
     *out_frame = resized(crop_region);
@@ -109,8 +109,13 @@ int main(int argc, char** argv) {
         // capture the next frame from the webcam
         camera >> frame;
 
+        size_t oh = frame.rows < frame.cols ? frame.rows : frame.cols;
+
+        cv::Mat cropped_original;
+        resize_and_crop(&frame, &cropped_original, oh, oh);
+
         cv::Mat cropped;
-        resize_and_crop(&frame, &cropped);
+        resize_and_crop(&frame, &cropped, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT);
 
         size_t feature_ix = 0;
         for (int rx = 0; rx < (int)cropped.rows; rx++) {
@@ -137,6 +142,19 @@ int main(int argc, char** argv) {
         }
 
     #if EI_CLASSIFIER_OBJECT_DETECTION == 1
+        // scaling factor between full-res video and output
+        float bb_factor = static_cast<float>(cropped_original.rows) / static_cast<float>(cropped.rows);
+
+        // this is BGR (not RGB)
+        cv::Scalar colors[] = {
+            cv::Scalar(75, 25, 230),
+            cv::Scalar(75, 180, 60),
+            cv::Scalar(25, 225, 255),
+            cv::Scalar(216, 99, 67),
+            cv::Scalar(49, 130, 245),
+        };
+        int max_colors = sizeof(colors) / sizeof(colors[0]);
+
         printf("Classification result (%d ms.):\n", result.timing.dsp + result.timing.classification);
         bool found_bb = false;
         for (size_t ix = 0; ix < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; ix++) {
@@ -144,6 +162,32 @@ int main(int argc, char** argv) {
             if (bb.value == 0) {
                 continue;
             }
+
+            int label_ix = 0;
+            for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+                if (strcmp(bb.label, ei_classifier_inferencing_categories[ix]) == 0) {
+                    label_ix = ((int)ix) % max_colors;
+                    break;
+                }
+            }
+
+            float bb_center_x = (static_cast<float>(bb.x) + (static_cast<float>(bb.width) * 0.5f)) * bb_factor;
+            float bb_center_y = (static_cast<float>(bb.y) + (static_cast<float>(bb.height) * 0.5f)) * bb_factor;
+
+            cv::Point bb_center(bb_center_x, bb_center_y);
+            cv::circle(cropped_original, bb_center, 3.0f * bb_factor, colors[label_ix], cv::LINE_AA, 0.5f * bb_factor);
+
+            double font_scale = 0.1f * bb_factor;
+            int thickness = 0.25f * bb_factor;
+            int baseline = 0;
+            cv::Size text_size = getTextSize(bb.label, cv::FONT_HERSHEY_SIMPLEX, font_scale, thickness, &baseline);
+
+            cv::Point text_coords(bb_center_x - (3.0f * bb_factor), bb_center_y - (4.5f * bb_factor));
+            cv::Point text_top_left(text_coords.x - (1.0f * bb_factor), text_coords.y - text_size.height - (1.0f * bb_factor));
+            cv::Point text_bottom_right(text_coords.x + text_size.width + (1.0f * bb_factor), text_coords.y + baseline + (1.0f * bb_factor));
+            cv::rectangle(cropped_original, text_top_left, text_bottom_right, colors[label_ix], -1);
+
+            cv::putText(cropped_original, bb.label, text_coords, cv::FONT_HERSHEY_SIMPLEX, font_scale, cv::Scalar(255, 255, 255), thickness);
 
             found_bb = true;
             printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
@@ -165,7 +209,7 @@ int main(int argc, char** argv) {
 
         // show the image on the window
         if (use_debug) {
-            cv::imshow("Webcam", cropped);
+            cv::imshow("Webcam", cropped_original);
             // wait (10ms) for a key to be pressed
             if (cv::waitKey(10) >= 0)
                 break;
