d76c6d49de315fe366e29e89dbb0e2b8744bdf32
Jan Jongboom <janjongboom@gmail.com>
2023-04-28 13:44:33 +0200

    OpenCV 4.7

--- build-opencv-mac.sh
+++ build-opencv-mac.sh
@@ -11,15 +11,17 @@ cd $OPENCV_DIR
 
 if [ ! -d "opencv" ]; then
     git clone https://github.com/opencv/opencv.git
-    cd opencv && git checkout 4.6.0 && cd ..
+    cd opencv && git checkout 4.7.0 && cd ..
 fi
 if [ ! -d "opencv_contrib" ]; then
     git clone https://github.com/opencv/opencv_contrib.git
-    cd opencv_contrib && git checkout 4.6.0 && cd ..
+    cd opencv_contrib && git checkout 4.7.0 && cd ..
 fi
 
 mkdir -p build_opencv
 cd build_opencv
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DBUILD_ZLIB=OFF ../opencv
 make -j
-make install
+
+echo "Installing libraries, this will prompt for sudo"
+sudo make install
