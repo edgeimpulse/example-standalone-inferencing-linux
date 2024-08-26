b2ed94bf1f7a8f9dcd454cc2a146de6a99f56761
Dmitry Maslov <dmitrywat@gmail.com>
2024-08-26 14:29:27 +0200

    bash script and readme fixes

--- tflite/download_flex_delegates.sh
+++ tflite/download_flex_delegates.sh
@@ -4,21 +4,21 @@ set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd $SCRIPTPATH/mac-x86_64
-if [ ! -f libtensorflowlite_flex_2.6.5.dylib ]; then
-    wget -O libtensorflowlite_flex_2.6.5.dylib --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/mac-x86_64/libtensorflowlite_flex_2.6.5.dylib
+if [ ! -f libtensorflowlite_flex_2.16.1.dylib ]; then
+    wget -O libtensorflowlite_flex_2.16.1.dylib --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/mac-x86_64/libtensorflowlite_flex_2.16.1.dylib
 fi
 
 cd $SCRIPTPATH/linux-armv7
-if [ ! -f libtensorflowlite_flex_2.6.5.so ]; then
-    wget -O libtensorflowlite_flex_2.6.5.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-armv7/libtensorflowlite_flex_2.6.5.so
+if [ ! -f libtensorflowlite_flex_2.16.1.so ]; then
+    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-armv7/libtensorflowlite_flex_2.16.1.so
 fi
 
 cd $SCRIPTPATH/linux-aarch64
-if [ ! -f libtensorflowlite_flex_2.6.5.so ]; then
-    wget -O libtensorflowlite_flex_2.6.5.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-aarch64/libtensorflowlite_flex_2.6.5.so
+if [ ! -f libtensorflowlite_flex_2.16.1.so ]; then
+    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-aarch64/libtensorflowlite_flex_2.16.1.so
 fi
 
 cd $SCRIPTPATH/linux-x86
-if [ ! -f libtensorflowlite_flex_2.6.5.so ]; then
-    wget -O libtensorflowlite_flex_2.6.5.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-x86/libtensorflowlite_flex_2.6.5.so
+if [ ! -f libtensorflowlite_flex_2.16.1.so ]; then
+    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-x86/libtensorflowlite_flex_2.16.1.so
 fi
--- tflite/linux-jetson-nano/README.md
+++ tflite/linux-jetson-nano/README.md
@@ -1,4 +1,10 @@
-The `libei_debug.a` file was built from: https://github.com/edgeimpulse/TensorRT/tree/libeitrt - build using:
+The `trt8.5.2/libei_debug.a` file was built from: https://github.com/edgeimpulse/TensorRT/tree/libeirt-tensorrt-8.5.2 - build using:
+
+```
+$ sh docker/build.sh --file docker/ubuntu-cross-aarch64.Dockerfile --tag tensorrt --cuda 11.8.0
+```
+
+The `trt8/libei_debug.a` file was built from: https://github.com/edgeimpulse/TensorRT/tree/libeirt - build using:
 
 ```
 $ sh docker/build.sh --file docker/ubuntu-cross-aarch64.Dockerfile --tag tensorrt --cuda 10.2
