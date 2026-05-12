1df34cb65f7f1146179f9335c7d6e4dd1d1cb7e5
Mateusz Majchrzycki <mateusz@edgeimpulse.com>
2026-05-12 11:27:13 +0200

    Add pre-built libs

--- tflite/download_flex_delegates.sh
+++ tflite/download_flex_delegates.sh
@@ -3,22 +3,35 @@ set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd $SCRIPTPATH/mac-x86_64
-if [ ! -f libtensorflowlite_flex_2.16.1.dylib ]; then
-    wget -O libtensorflowlite_flex_2.16.1.dylib --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/mac-x86_64/libtensorflowlite_flex_2.16.1.dylib
+cd $SCRIPTPATH/linux-aarch64
+if [ ! -f libtensorflowlite_flex_2.19.0.so ]; then
+    wget -O libtensorflowlite_flex_2.19.0.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-aarch64/libtensorflowlite_flex_2.19.0.so
+fi
+if [ ! -f libtensorflowlite_gpu_delegate.so ]; then
+    wget -O libtensorflowlite_gpu_delegate.so --show-progress https://cdn.edgeimpulse.com/build-system/gpu-delegates/linux-aarch64/libtensorflowlite_gpu_delegate.so
 fi
 
 cd $SCRIPTPATH/linux-armv7
-if [ ! -f libtensorflowlite_flex_2.16.1.so ]; then
-    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-armv7/libtensorflowlite_flex_2.16.1.so
+if [ ! -f libtensorflowlite_flex_2.19.0.so ]; then
+    wget -O libtensorflowlite_flex_2.19.0.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-armv7/libtensorflowlite_flex_2.19.0.so
 fi
 
-cd $SCRIPTPATH/linux-aarch64
+cd $SCRIPTPATH/linux-armv7-legacy
 if [ ! -f libtensorflowlite_flex_2.16.1.so ]; then
-    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-aarch64/libtensorflowlite_flex_2.16.1.so
+    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-armv7-legacy/libtensorflowlite_flex_2.16.1.so
 fi
 
 cd $SCRIPTPATH/linux-x86
-if [ ! -f libtensorflowlite_flex_2.16.1.so ]; then
-    wget -O libtensorflowlite_flex_2.16.1.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-x86/libtensorflowlite_flex_2.16.1.so
+if [ ! -f libtensorflowlite_flex_2.19.0.so ]; then
+    wget -O libtensorflowlite_flex_2.19.0.so --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/linux-x86/libtensorflowlite_flex_2.19.0.so
+fi
+
+cd $SCRIPTPATH/mac-arm64
+if [ ! -f libtensorflowlite_flex_2.19.0.dylib ]; then
+    wget -O libtensorflowlite_flex_2.19.0.dylib --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/mac-arm64/libtensorflowlite_flex_2.19.0.dylib
+fi
+
+cd $SCRIPTPATH/mac-x86_64
+if [ ! -f libtensorflowlite_flex_2.19.0.dylib ]; then
+    wget -O libtensorflowlite_flex_2.19.0.dylib --show-progress https://cdn.edgeimpulse.com/build-system/flex-delegates/mac-x86_64/libtensorflowlite_flex_2.19.0.dylib
 fi
